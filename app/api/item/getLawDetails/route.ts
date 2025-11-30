75f8436e8624fb776d0fb399509adcf6cc42bf45
projects <yuuki.kuma0401@gmail.com>
2025-11-30 19:53:52 +0900

    fix api

--- app/api/item/getLawDetails/route.ts
+++ app/api/item/getLawDetails/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from "next/server";
-type Law_id = {
-  law_id: string
-}
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Law_id){
+
+export async function POST(request: NextRequest){
+  const reqBody = await request.json()
+  const law_id = reqBody.law_id
   try{
-    const response = await fetch(`https://laws.e-gov.go.jp/api/2/law_data/${request.law_id}`)
+    const response = await fetch(`https://laws.e-gov.go.jp/api/2/law_data/${law_id}`)
     const data = await response.json()
     return NextResponse.json({message: "法令本文取得成功", data: data})
   }catch(err){
