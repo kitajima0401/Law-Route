5a1bc523ebe09cf59d09db9eae58326e4d5b533c
projects <yuuki.kuma0401@gmail.com>
2025-12-09 06:30:53 +0900

    add route.ts

--- app/api/item/readSingle/[law_id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(request: NextRequest){
-  try{
-    return NextResponse.json({message: "laws取得成功"})
-  }catch{
-    return NextResponse.json({message: "laws取得失敗"})
-  }
-}
