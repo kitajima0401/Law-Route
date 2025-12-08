81f0995f6d49253bd367dff2c87f895ba91e3dd5
projects <yuuki.kuma0401@gmail.com>
2025-12-08 23:12:54 +0900

    add console.log

--- app/api/user/register/route.ts
+++ app/api/user/register/route.ts
@@ -7,13 +7,17 @@ export async function POST(request: NextRequest){
   const reqBody = await request.json()
   try{
     await connectDB()
+    console.log("データベース接続成功")
   }catch{
+    console.log("データベース接続失敗")
     return NextResponse.json({massege: "データベース接続失敗"})
   }
   try{
     const savedUserEmail = await UserModel.findOne({email: reqBody.email})
     if(savedUserEmail){
       return NextResponse.json({message: "該当メールアドレスのユーザーは既に存在します"})
+    }else{
+      console.log("登録中...")
     }
     const hashedPassword = await bcrypt.hash(reqBody.password, 10)
     await UserModel.create({
@@ -21,6 +25,7 @@ export async function POST(request: NextRequest){
       email: reqBody.email,
       password: hashedPassword
     })
+    console.log("パスワードハッシュ化成功")
     return NextResponse.json({message: "ユーザー登録成功"})
   }catch(err){
     return NextResponse.json({message: "ユーザー登録失敗"})
