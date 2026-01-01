3cfbf9ba29f65a241031473770798c8ddbed2420
projects <yuuki.kuma0401@gmail.com>
2026-01-01 23:04:14 +0900

    fix header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,24 @@
 "use client"
 import {  Typography,  Button,  AppBar, Toolbar } from "@mui/material"
 import useAuth from "../api/utils/useAuth"
+import { usePathname } from "next/navigation"
 
 const Header = () => {
   const {loginUserEmail, logout} = useAuth()
+  const pathName = usePathname()
   return(
     <AppBar position="fixed" color="default" >
       <Toolbar>
-        <Typography component="a" variant="h6" href="/" flexGrow={1} color="default">
-          LawRoute
-        </Typography>
+        
+        {pathName === "/mypage"?(
+          <Typography component="a" variant="h6" href="/" flexGrow={1} color="default">
+            LawRoute/mypage
+          </Typography>
+        ):(
+          <Typography component="a" variant="h6" href="/" flexGrow={1} color="default">
+            LawRoute
+          </Typography>
+        )}
         {!loginUserEmail?(
           <>
             <Button color="inherit" href="/login">
